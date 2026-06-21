acc282a310725fd6730ea5cc4e46e0d52af083b7
multibulgarov1 <multibulgarov1@gmail.com>
2026-06-21 15:41:27 +0300

    first version

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from settings import WIDTH, HEIGHT
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
